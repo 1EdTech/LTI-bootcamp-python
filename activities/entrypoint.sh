84af6e26b9031c606170e438d91c2510d8fea2b8
tcouper <tim.couper@gmail.com>
2024-10-20 08:52:40 +0100

    fixes for windows

--- activities/entrypoint.sh
+++ activities/entrypoint.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# Default to exercise1 if no parameter is provided
-ACTIVITY=${ACTIVITY_NUMBER:-1}
-
-# Navigate to the specified exercise directory
+ACTIVITY=${ACTIVITY_NUMBER:-4}
 cd /usr/src/app/activities/activity$ACTIVITY
 
 echo "Running exercise activity$ACTIVITY"
 
-# Run the exercise's main Python script
-python app.py
+python /usr/src/app/activities/activity$ACTIVITY/app.py
